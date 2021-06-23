7cb22cb220686756042239d20b0d3f5d25cc2dd1
Mateus Augusto <85250525+Hyouka-Japa@users.noreply.github.com>
2021-06-23 09:15:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+//usando o arquivo organizador 
 const organizador = require('./organizar')
 
+//organizado string nivel 1
 console.log(organizador.organizar('bAnaNa'))
+//organizado array nivel 2
 console.log(organizador.organizarArray(['bAnaNa']))
+//organizado frase nivel 3
 console.log(organizador.organizarMaiusculo('bAnAnA NAnAN'))
