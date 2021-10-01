77424b61809ba76b6464fdc806f1223d714f0eaf
Mateus Augusto <85250525+Mateus-Augusto-AC@users.noreply.github.com>
2021-10-01 17:37:27 -0300

    Update organizador.js

--- organizador.js
+++ organizador.js
@@ -16,10 +16,10 @@ module.exports.organizarArray = palavras => {
 
 //nivel 3
 module.exports.organizarMaiusculo = text => {
-	let words = text.toLowerCase().split(" ");
-    for (let a = 0; a < words.length; a++) {
-        let w = words[a];
-        words[a] = w[0].toUpperCase() + w.slice(1);
-    }
-    return words.join(" ");
+   let words = text.toLowerCase().split(" ")
+       for (let a = 0; a < words.length; a++) {
+	    let w = words[a]
+	    words[a] = w[0].toUpperCase() + w.slice(1)
+       }
+    return words.join(" ")
 }    
